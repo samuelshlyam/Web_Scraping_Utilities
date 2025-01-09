967bd7fc65730fcf14af86df852397c601387d62
samuelshlyam <sammich5302@gmail.com>
2025-01-08 19:09:21 -0500

    Moose Knuckles Settings Change for new site

--- Web_Scraping_Utility/main.py
+++ Web_Scraping_Utility/main.py
@@ -496,7 +496,7 @@ def process_brand(expander, brand_name, data, current_directory):
 if __name__ == "__main__":
     with open("settings.json", "r") as file:
         jsonData = json.load(file)
-    brand="Moose Knuckles"
+    brand="Versace"
     data=jsonData[brand]
     expander=PageExpander()
     process_brand(expander,brand,data,current_directory)
